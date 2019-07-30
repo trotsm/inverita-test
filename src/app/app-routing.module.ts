18539fd9e0ca8163cc3057ddb5b73cf0a462ed46
trotsm <trots86@gmail.com>
2019-07-30 18:02:31 +0300

    initial commit.Basic task is made.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageThreeComponent } from './page-three/page-three.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
-const routes: Routes = [];
+const appRouts: Routes = [
+  {path: '', component: PageOneComponent},
+  {path: 'page2', component: PageTwoComponent},
+  {path: 'page3', component: PageThreeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRouts)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
